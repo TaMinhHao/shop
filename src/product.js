4f70a4cf81e4b559eea6b05201f48eb0ce8ab143
TaMinhHao <kuahru29102000@gmail.com>
2023-08-06 17:32:33 +0700

    first commit

---
+++ src/product.js
@@ -0,0 +1,34 @@
+import product1 from "./assets/bandmember.jpg"
+
+export const PRODUCTS =[
+    {
+        id: 1,
+        productName: "Iphone",
+        price: 999.0,
+        productImage:  product1,
+    },
+    {
+        id: 2,
+        productName: "Iphone1",
+        price: 999.0,
+        productImage:  product1,
+    },
+    {
+        id: 3,
+        productName: "Iphone2",
+        price: 999.0,
+        productImage:  product1,
+    },
+    {
+        id: 4,
+        productName: "Iphone3",
+        price: 999.0,
+        productImage:  product1,
+    },
+    {
+        id: 5,
+        productName: "Iphone4",
+        price: 999.0,
+        productImage:  product1,
+    },
+]
